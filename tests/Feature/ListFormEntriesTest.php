9ae839c1153d8e2cd283bb27e9d13cf874f1d4be
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-06-22 22:11:39 -0400

    Create ListFormEntriesTest.php

---
+++ tests/Feature/ListFormEntriesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use App\Models\Form;
+use App\Models\FormEntry;
+use Tests\TestCase;
+
+class ListFormEntriesTest extends TestCase
+{
+    public function test_user_can_see_form_entry_list_screen()
+    {
+        $user = User::factory()->create();
+        $form = Form::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->actingAs($user)->get("/forms/{$form->id}");
+
+        $response->assertStatus(200);
+    }
+    
+    public function test_guest_cannot_see_form_entry_list_screen()
+    {
+        $user = User::factory()->create();
+        $form = Form::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get("/forms/{$form->id}");
+
+        $response->assertRedirect('/login');
+    }
+
+    public function test_user_can_list_all_their_form_entries()
+    {
+        
+    }
+
+    public function test_user_cannot_list_all_the_form_entries_of_others()
+    {
+        
+    }
+}
