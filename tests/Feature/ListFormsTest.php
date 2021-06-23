013b9241244ed0a773c18a17a13125d872a24d01
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-06-22 23:05:34 -0400

    Act as user in test_user_can_see_form_list_screen()

--- tests/Feature/ListFormsTest.php
+++ tests/Feature/ListFormsTest.php
@@ -4,13 +4,16 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
 use Tests\TestCase;
 
 class ListFormsTest extends TestCase
 {
     public function test_user_can_see_form_list_screen()
     {
-        $response = $this->get('/forms');
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/forms');
 
         $response->assertStatus(200);
     }
