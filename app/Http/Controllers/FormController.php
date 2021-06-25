b4b29426c61d8f6793aba119e021712450a4a9cb
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-06-24 23:18:42 -0400

    Add controller methods

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class FormController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Form/Index', [
+            'forms' => Auth::user()->forms()
+                ->paginate(10),
+        ]);
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function destroy()
+    {
+        
+    }
 }
