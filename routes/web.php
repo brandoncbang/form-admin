fe6d91a258c0bf07ec892d25c4d25404de604cda
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-06-26 21:28:05 -0400

    Add all Form routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,26 @@ use Inertia\Inertia;
 */
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('/forms', [FormController::class, 'index'])->name('form.index');
-    Route::get('/forms/create', [FormController::class, 'create'])->name('form.create');
+    Route::get('/forms', [FormController::class, 'index'])
+        ->name('form.index');
+
+    Route::get('/forms/new', [FormController::class, 'create'])
+        ->name('form.create');
+
+    Route::post('/forms', [FormController::class, 'store'])
+        ->name('form.store');
+
+    Route::get('/forms/{id}', [FormController::class, 'show'])
+        ->name('form.show');
+
+    Route::get('/forms/{id}/settings', [FormController::class, 'edit'])
+        ->name('form.edit');
+
+    Route::put('/forms/{id}', [FormController::class, 'update'])
+        ->name('form.update');
+
+    Route::delete('/forms/{id}', [FormController::class, 'destroy'])
+        ->name('form.destroy');
 });
 
 Route::get('/', function () {
