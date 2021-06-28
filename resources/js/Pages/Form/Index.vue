a040dd5f7578ae048c2971b893533f5cbb3911a9
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-06-27 22:39:30 -0400

    Show loading state in infinite scroll

--- resources/js/Pages/Form/Index.vue
+++ resources/js/Pages/Form/Index.vue
@@ -7,22 +7,31 @@
     <div
       class="flex flex-col flex-1 w-full min-h-0 py-8 mx-auto sm:py-12 max-w-7xl sm:px-6 lg:px-8"
     >
-      <ul ref="scrollContainer" class="flex-1 overflow-auto">
-        <li
-          class="border-t first:border-t-0"
-          v-for="form in allForms.data"
-          :key="form.id"
-        >
-          <inertia-link
-            class="block px-6 py-4 bg-white group focus:outline-none hover:outline-none"
-            :href="route('form.show', { id: form.id })"
+      <div ref="scrollContainer" class="flex-1 overflow-auto">
+        <ul>
+          <li
+            class="border-t first:border-t-0"
+            v-for="form in allForms.data"
+            :key="form.id"
           >
-            <h3 class="text-lg font-semibold text-gray-800 group-focus:underline group-hover:underline">
-              {{ form.name }}
-            </h3>
-          </inertia-link>
-        </li>
-      </ul>
+            <inertia-link
+              class="block px-6 py-4 bg-white group focus:outline-none hover:outline-none"
+              :href="route('form.show', { id: form.id })"
+            >
+              <h3 class="text-lg font-semibold text-gray-800 group-focus:underline group-hover:underline">
+                {{ form.name }}
+              </h3>
+            </inertia-link>
+          </li>
+        </ul>
+
+        <div
+          v-if="allForms.next_page_url"
+          class="px-6 py-4 text-center bg-white"
+        >
+          Loading...
+        </div>
+      </div>
 
       <div class="flex items-center justify-between px-6 mt-8 sm:px-0">
         <div class="text-gray-600">
