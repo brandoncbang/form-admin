ef0972bc0675f1dc94453a4f9698f3739a65fc0f
thermalsquid <36747510+thermalsquid@users.noreply.github.com>
2021-07-14 23:17:02 -0400

    Create possible controller for single form entry

---
+++ app/Http/Controllers/FormEntryContoller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormEntryContoller extends Controller
+{
+    public function show()
+    {
+        // Mark read_at
+        // Return data for ajax request
+    }
+}
